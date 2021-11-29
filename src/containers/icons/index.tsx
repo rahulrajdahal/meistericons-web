acea6ddc87746243d0ce0aa91d1322feb38cd303
Markaa10 <rahulrajdahal@gmail.com>
2021-11-29 17:54:42 +0545

    icons

--- src/containers/icons/index.tsx
+++ src/containers/icons/index.tsx
@@ -3,17 +3,37 @@ import styled from "styled-components";
 import * as icons from "meistericons";
 
 export const Container = styled.section`
-  width: 80%;
-  margin: 0 auto;
+  width: 100%;
+  margin: 4.3rem auto;
+
+  display: grid;
+  grid-template-columns: repeat(8, minmax(1.6rem, 1fr));
+  gap: 4.1rem;
+
+  @media (min-width: 1368px) {
+    width: 30%;
+  }
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 26px;
+  height: auto;
+
+  cursor: pointer;
 `;
 
 function IconsContainer() {
-  const keys = Object.keys(icons);
   const values = Object.values(icons);
 
   return (
     <Container>
-      <icons.ArchiveIcon />
+      {values.map((item, i) => (
+        <IconContainer key={i}> {React.createElement(item)}</IconContainer>
+      ))}
     </Container>
   );
 }
