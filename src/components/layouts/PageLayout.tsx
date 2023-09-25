7ba86d0ee991b30f76a9cdd710483c65ede8cde5
rahulrajdahal <rahulrajdahal@gmail.com>
2023-09-25 11:29:27 +0545

    fix(hooks): update with bun

--- src/components/layouts/PageLayout.tsx
+++ src/components/layouts/PageLayout.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
-import { useNavPosition } from '@/hooks/useNavPosition';
 
 export default function PageLayout({ children }: { children: React.ReactNode }) {
   return (
